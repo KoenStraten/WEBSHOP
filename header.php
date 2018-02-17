ab6d2330449e0e60d54a2969b5352e8141f3dbbe
koenstraten <kstraten@avans.nl>
2018-02-17 15:17:46 +0100

    kleine aanpassing

--- header.php
+++ header.php
@@ -24,7 +24,7 @@ img {
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
-				<li class="nav-item active"><a class="nav-link" href="/workspace/webshop/webshop/home.php">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="/workspace/webshop/webshop/home.php">Home</a></li>
 				
 				<li class="nav-item dropdown"><a class="nav-link dropdown-toggle"
 					href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
