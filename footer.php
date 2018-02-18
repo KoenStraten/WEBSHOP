9aafc62bd3351b2cd35a2a235bf0b0fb9dbcc86c
koenstraten <kstraten@avans.nl>
2018-02-18 20:10:55 +0100

    weet nog steeds niet of dit werkt

--- footer.php
+++ footer.php
@@ -5,10 +5,25 @@
 </head>
 
 <body>
-	<div class="card-footer text-muted">
+	<footer class="card-footer text-muted">
+		<div class="pull-right">
+			<ul class="list-inline float-left pr-3">
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">About</a></li>
+			</ul>
+			<ul class="list-inline float-left pr-3">
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Twitter</a></li>
+				<li><a href="#">Instagram</a></li>
+			</ul>
+			<ul class="list-inline">
+				<li><a href="#">Nog meer</a></li>
+				<li><a href="#">Etc</a></li>
+				<li><a href="#">Etc</a></li>
+			</ul>
+		</div>
 		<p>Copyright © 1995-2018 Webshop Inc. All Rights Reserved.</p>
-	</div>
-
+	</footer>
 </body>
 
 </html>
--- product.php
+++ product.php
@@ -7,7 +7,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <style type="text/css">
-
 </style>
 </head>
 
@@ -18,8 +17,46 @@ require_once 'header.php';
 </header>
 
 <body>
-<h3>Product</h3>
-	<!-- scripts  -->
+	<div class="container">
+		<h3 class="pt-3">Productnaam</h3>
+		<img style="float: left; width: 23%"
+			src="http://www.clker.com/cliparts/c/2/4/3/1194986855125869974rubik_s_cube_random_petr_01.svg.med.png"
+			class="img-fluid mr-5 mt-3" alt="Cinque Terre"> <br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+			aliquip ex ea commodo consequat. Duis aute irure dolor in
+			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+			culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+			dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+			commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing
+			elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+			aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+			dolor in reprehenderit in voluptate velit esse cillum dolore eu
+			fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est
+			laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+		<h3 class="pt-3">Productgegevens enzo</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+			eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+			ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+			aliquip ex ea commodo consequat. Duis aute irure dolor in
+			reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+			culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+			dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+
+		<h3 class="pt-3">Reviews</h3>
+		<h3 class="pt-3 pb-3">Statistics</h3>
+
+		<!-- scripts  -->
+	</div>
 	<script
 		src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">
 	</script>
