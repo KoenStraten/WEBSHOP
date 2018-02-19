3e56cb8f1d3cd23f6b5dd867af1b8a4ebd9ee8d3
koenstraten <kstraten@avans.nl>
2018-02-19 15:20:10 +0100

    commit met nieuw klassen

---
+++ model/customer.php
@@ -0,0 +1,9 @@
+<?php
+
+class Customer
+{
+
+    function __construct()
+    {}
+}
+?>
---
+++ model/review.php
@@ -0,0 +1,24 @@
+<?php
+
+class Review
+{
+
+    private $text;
+    private $rating;
+    private $title;
+    private $customer;
+    private $product_id;
+    
+    function __construct($dbReview)
+    {
+        $this->text = $dbReview->text;
+        $this->rating = $dbReview->rating;
+        $this->title = $dbReview->title;
+        $this->customer = $dbReview->text;
+        // find customer by id
+        $this->product_id = $dbReview->product_id;  
+    }
+    
+    
+}
+?>
