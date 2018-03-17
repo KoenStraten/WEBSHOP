d8069c22283973f98e5d4d2ee9cf8a2fd38895d0
TomVerkuijlen <tljverku@avans.nl>
2018-03-17 14:35:14 +0100

    migration and started on views

--- webshopv2/resources/views/pages/category.blade.php
+++ webshopv2/resources/views/pages/category.blade.php
@@ -3,19 +3,13 @@
     <div class="container">
         <h3>{{ $category }}</h3>
         <div class="my-3 p-3 bg-white rounded box-shadow">
-            <?php
-            $pcounter = 0;
-            $productCount = count($products);
-            ?>
             @foreach ($products as $p)
-                <?php
-                $pcounter++;
-                if ($pcounter < $productCount) {
-                    echo "<div class='row productline'>";
-                } else {
-                    echo "<div class='row lastline'>";
-                }
-                ?>
+
+                @if (!$loop->last)
+                    <div class='row productline'>
+                @else
+                    <div class='row lastline'>
+                @endif
                 <div class="col-md-4">
                     <a href="../product/{{ $p->id }}">
                         <img src="{{ $p->image }}" style="max-height: 200px">
@@ -24,20 +18,14 @@
                 <div class="col-md-6">
                     <h4><a class="text-dark" href="../product/{{ $p->id }}">{{ $p->name }}</a></h4>
                     <p>{{ $p->description  }} <br><br>
-                    <?php
-                    $reviews = \App\Review::getAllById($p->id);
-                    $rating = \App\Product::getReviewScore($p->id);
-                    $counter = 0;
-                    while ($counter < $rating) {
-                        echo "<span class='fa fa-star checked'></span>";
-                        $counter++;
-                    }
-                    while ($counter < 5) {
-                        echo "<span class='fa fa-star unchecked'></span>";
-                        $counter++;
-                    }
-                    echo "<span class='card-text'> ( " . count($reviews) . " )</span>"
-                    ?>
+                    @for($i = 0; $i < 5; $i++)
+                        @if($i < $p->rating())
+                                <span class="fa fa-star checked"></span>
+                        @else
+                                <span class="fa fa-star unchecked"></span>
+                        @endif
+                    @endfor
+                        <span class="card-text">{{ " ( " . count($p->reviews) . " )" }}</span>
                 </div>
                 <div class="col-md-2">
                     <p class="price">{{ "$" . $p->price }}</p>
