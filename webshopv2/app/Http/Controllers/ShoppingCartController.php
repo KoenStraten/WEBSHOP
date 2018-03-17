a4c35d054bf09b1c5299c76bcc64dbb0df8c7927
koenstraten <kstraten@avans.nl>
2018-03-17 14:35:47 +0100

    leggo

--- webshopv2/app/Http/Controllers/ShoppingCartController.php
+++ webshopv2/app/Http/Controllers/ShoppingCartController.php
@@ -2,35 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+use App\ProductInCart;
 use App\ShoppingCart;
-use Illuminate\Http\Request;
 
 class ShoppingCartController extends Controller
 {
 
     public function index()
     {
-        //$productsInCarts = Product::all();
-        return view('pages/shoppingcart', compact('productsInCarts'));
+
     }
 
     public function store()
     {
-        $this->validate(request(), [
-            'amount' => 'required',
-        ]);
-
         $product_id = request('product');
+        $amount = request('amount');
 
-//        $review = new Review();
-//        $review->title = request('titel');
-//        $review->rating = request('star');
-//        $review->text = request('content');
-//        $review->user_id = Auth::user()->id;
-//        $review->product_id = $product_id;
+        $user = \Illuminate\Support\Facades\Auth::user();
+        $carts = $user->shoppingCarts->where('paid', '0')->where('user_id', $user->id);
 
-//        $review->save();
+        if (count($carts) == 0) {
+            $shoppingCart = new ShoppingCart();
+            $shoppingCart->user_id = $user->id;
+            $totalCost = 0;
+            $shoppingCart->total_cost = $totalCost;
+            $shoppingCart->save();
+        } else {
+            foreach ($carts as $cart) {
+                $totalCost = $cart->total_cost;
+                $shoppingCart = $cart;
+            }
+        }
+
+        $product = Product::find($product_id);
+        $price = $product->price;
+
+        $counter = 0;
+        while ($counter < $amount) {
+            $productInCart = new ProductInCart();
+            $totalCost = $totalCost + $price;
+            $productInCart->product_id = $product_id;
+            $carts = $user->shoppingCarts->where('paid', '0')->where('user_id', $user->id);
+
+            $counter++;
+        }
+        $shoppingCart->save();
 
         return redirect('/product/' . $product_id);
+    }
+
+    public function show()
+    {
+        $user = \Illuminate\Support\Facades\Auth::user();
+
+        $carts = $user->shoppingCarts->where('paid', '0');
+
+        $productsInCarts = array();
+
+        foreach ($carts as $cart) {
+            echo $productIds = $cart->ProductsInCart();
+        }
+
+//        foreach ($productIds as $productId) {
+//            $product = Product::find($productId);
+//            array_push($productsInCarts, $product);
+//        }
+        return view('pages/shoppingcart', compact('productsInCart'));
     }
 }
