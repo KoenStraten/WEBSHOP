b356071d2bf63a4342e530d1272dc205bd70ebd8
TomVerkuijlen <tljverku@avans.nl>
2018-03-26 13:28:40 +0200

    changed role of registered user

--- webshopv2/app/Http/Controllers/Auth/RegisterController.php
+++ webshopv2/app/Http/Controllers/Auth/RegisterController.php
@@ -80,7 +80,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role' => 'gast',
+            'role' => 'gebruiker',
         ]);
     }
 }
