5f76d7683fdef97f175b74a7cb95917a2ff9200a
koenstraten <kstraten@avans.nl>
2018-03-31 13:28:08 +0200

    index update

--- webshopv2/public/index.php
+++ webshopv2/public/index.php
@@ -36,6 +36,8 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
+// require __DIR__.'/bootstrap/autoload.php';
+// $app = require_once __DIR__.'/bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
